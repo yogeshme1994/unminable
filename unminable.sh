d1b34ef39aed5fc2447128ee0cbcbe0f09335048
yogeshme1994 <75430476+yogeshme1994@users.noreply.github.com>
2021-12-10 14:42:09 +0530

    Create unminable.sh

---
+++ unminable.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
+tar xf xmrig-6.14.0-linux-x64.tar.gz
+cd xmrig-6.14.0
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TUFgJxv9jM7gaQ3vYhGvHiyZUeS6icohdQ.YOGI_Miner#e0cg-wfrb -p x --cpu 4
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
